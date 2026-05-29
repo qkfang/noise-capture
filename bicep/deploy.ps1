9a27477efa9bcccadf27e232984d8ef1fcac36e8
Daniel Fang <qkfang@hotmail.com>
2026-05-29 12:58:51 +1000

    add

--- bicep/deploy.ps1
+++ bicep/deploy.ps1
@@ -3,8 +3,8 @@ az group create --name 'rg-noise-capture' --location 'westus3'
 
 az deployment group create --name 'noise-capture-deploy' --resource-group 'rg-noise-capture' --template-file './main.bicep' --parameters './main.bicepparam'
 
-$spAppId = az ad sp list --display-name 'sp-demo-01' --query '[0].appId' -o tsv
+$spObjectId = 'a6efe236-83c5-472b-a068-65006e369ad7'  # sp-demo-01
 $subscriptionId = az account show --query 'id' -o tsv
-az role assignment create --assignee $spAppId --role 'Contributor' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-noise-capture"
-az role assignment create --assignee $spAppId --role 'User Access Administrator' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-noise-capture"
-    
+az role assignment create --assignee-object-id $spObjectId --assignee-principal-type ServicePrincipal --role 'Contributor' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-noise-capture"
+az role assignment create --assignee-object-id $spObjectId --assignee-principal-type ServicePrincipal --role 'User Access Administrator' --scope "/subscriptions/$subscriptionId/resourceGroups/rg-noise-capture"
+
